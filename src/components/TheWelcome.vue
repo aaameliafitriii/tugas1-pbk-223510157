976333a6d92bd31c2ce721d4af8483fa74160c10
amel <ameliaftr0912@gmail.com>
2024-04-26 11:40:31 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="container">
+    <header>
+      <h1>CURICULUM VITAE</h1>
+      <p>web Developer</p>
+    </header>
+    <hr>
+    <section>
+      <h2>About Me</h2>
+      <p>i'm a passinat  web developer with experience in HTML,CSS , and Javascript.  I Love creating responsive</p>
+    </section>
+    <section>
+      <h2>experince</h2>
+      <h3>Web developer - XYZ Company</h3>
+      <p>Developed and maintained company websites.collaborated with designers to implement user-friendly interface</p>
+    </section>
+    <section>
+      <h2> Education</h2>
+      <h3>Bachelor's Degree in informatics engineering - RIAU ISLAMIC UNVERSITY</h3>
+      <p>Graduated with honors.</p>
+    </section>
+    <section>
+      <h2>skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+      </ul>
+    </section>
+    <footer>
+      <p>Contect me at : Ameliaftitri216@student.uir.ac.id</p>
+    </footer>
+    </div>
+    </template>
+
+    <style>
+    body {
+      font-size: arial, sans-serif;
+      background-color: rgb(60, 223, 223);
+      margin: 0;
+      padding: 0;
+    }
+    .container {
+      max-width: 800px;
+      margin: 50px auto;
+      background-color: rgb(224, 140, 13);
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgb(224, 140, 13);
+    }
+    h1,h2,h3{
+      color:aqua
+    }
+        p {
+        color: bisque;
+      }
+  </style>
+  
+    
+   
